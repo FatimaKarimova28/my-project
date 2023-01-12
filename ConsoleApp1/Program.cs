fab54e61a38228373aa1ce10daaadec82e83faea
Fatima I. Karimova <fatimaika@code.local>
2023-01-12 21:53:35 +0400

    my project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int number = 34;
+            //        if (number % 3 ==0 && number % 5 == 0 )
+            //    {
+            //        Console.WriteLine("yes");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Noo") ;
+            //    }
+
+
+
+            // int amount = 100;
+            //int  price = 23;
+            // if (amount >= price)
+            // {
+            //     Console.WriteLine("You can buy");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Sorry go to work");
+            // }
+
+
+            //int math = 71;
+            //    int music = 91;
+            //    int litrecy = 79;
+            //    int logic = 66;
+            //    int biology = 99;
+            //if ((math + litrecy + logic + biology + music) / 5 > 65)
+            //{
+            //    Console.WriteLine("You pass your exam");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Soory try again");
+            //}
+
+
+            //int number1 = 23;
+            //int number2 = 56;
+            //    if (number1 > number2)
+            //{
+            //    Console.WriteLine("Number 1 is bigger then number2");
+            //}
+            //    else if (number1 == number2)
+            //{
+            //    Console.WriteLine("Number1 = Number2");
+            //}
+
+            //    else
+            //{
+            //    Console.WriteLine("Number2 is bigger then Number1");
+            //}
+            //{
+
+            //int temperature = 0;
+            //if (temperature == 0)
+            //{
+            //    Console.WriteLine("temperature is freeze");
+
+            //}
+            //else if (temperature < 0 )
+            //{
+            //    Console.WriteLine("tempoerature under 0");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not freeze");
+            //}
+
+
+            //int number = 50;
+
+            //for (int i = 0; i <= number; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int count = 0;
+
+            //for (int i = 0; i < 40; i++)
+            //{
+            //    if (i% 3 == 0)
+            //    {
+            //        count++;
+            //    }
+
+
+            //}
+            //Console.WriteLine(count);
+
+
+
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 3 == 0 && i % 7 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+
+            //int count = 0;
+            //int number = 1234;
+            //while (number > 0)
+            //{
+            //    number /= 10;
+            //    count++;
+
+
+            //}
+            //Console.WriteLine(count);
+
+
+
+
+
+            //int number = 1234;
+            //int reminder = 0;
+            //int reservedNumber = 0;
+            //while (number > 0)
+            //{
+            //    reminder = number % 10;
+            //    reservedNumber = (reservedNumber * 10) + reminder;
+            //        number /= 10;
+            //}
+            //Console.WriteLine(reservedNumber);
+
+
+            //int number = 1;
+            //int faktorial = 10;
+
+            //for (int i = 1; i <= faktorial; i++)
+            //{
+            //    number *= i;
+            //}
+            //Console.WriteLine(number);
+
+
+            //            int number = 1;
+            //            int faktorial = 1;
+            //while (faktorial <= 10)
+            //            {
+            //                number *= faktorial;
+            //                faktorial++;
+
+            //            }
+            //            Console.WriteLine(number);
+
+
+            //int number = 3;
+            //bool isPrime = true; 
+
+            //for (int i = 2; i < number; i++)
+            //{
+            //    if (number % i == 0)
+            //    {
+            //        isPrime = false;
+            //        break;
+            //    }
+            //}
+
+            //if (isPrime == true)
+            //{
+            //    Console.WriteLine("sade");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("MUREKKEB");
+            //}
+
+
+
+            //int number = 1234;
+            //    int sum = 0;
+            //int reminder = 0;
+            //while ( number > 0)
+            //{
+            //    reminder = number % 10;
+            //        sum += reminder;
+            //    number /= 10;
+            //}
+            //Console.WriteLine(sum);
+
+
+        }
+
+    }
+}
+
+
